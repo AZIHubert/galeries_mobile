6a0456dbcd4cb6baa250cf2fa530ff633be0e651
AZIHubert <allan.aoudji@protonmail.com>
2021-02-03 12:13:57 +0100

    add quit button

--- src/routes/ImageView/index.tsx
+++ src/routes/ImageView/index.tsx
@@ -1,10 +1,15 @@
+import Constants from 'expo-constants';
+import { AntDesign } from '@expo/vector-icons';
 import * as React from 'react';
 import {
+  TouchableOpacity,
   ScrollView,
   StyleSheet,
+  View,
 } from 'react-native';
 
-import profilePicture from '#ressources/images/mockImages/profilePicture10.jpg';
+import theme from '#helpers/theme';
+import profilePicture from '#ressources/images/mockImages/profilePicture9.jpg';
 
 import FullPageImage from './FullPageImage';
 import Informations from './Informations';
@@ -22,31 +27,66 @@ const ImageView = () => {
   };
 
   return (
-    <ScrollView
-      ref={scrollView}
-      contentContainerStyle={styles.contentContainer}
-      style={styles.scrollView}
+    <View
+      style={{ flex: 1 }}
     >
-      <FullPageImage
-        onPress={scollToBottom}
-        source={profilePicture}
-      />
-      <Informations
-        onPress={scrollToTop}
-        size='2000x3000'
-        upload='24th Septembre 2020'
-        weight='6ko'
-      />
-    </ScrollView>
+      <ScrollView
+        stickyHeaderIndices={[0]}
+        ref={scrollView}
+        contentContainerStyle={styles.contentContainer}
+        style={styles.scrollView}
+      >
+        <View
+          style={styles.container}
+        >
+          <TouchableOpacity
+            activeOpacity={theme.touchableOpacity.defaultOpacity}
+            style={styles.buttonContainer}
+          >
+            <AntDesign
+              color={theme.color.primary}
+              name="closecircle"
+              size={30}
+            />
+          </TouchableOpacity>
+        </View>
+        <FullPageImage
+          onPress={scollToBottom}
+          source={profilePicture}
+        />
+        <Informations
+          onPress={scrollToTop}
+          size='2000x3000'
+          upload='24th Septembre 2020'
+          weight='6ko'
+        />
+      </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    alignItems: 'center',
+    backgroundColor: theme.color.secondary,
+    borderRadius: 25,
+    height: 30,
+    justifyContent: 'center',
+    width: 30,
+  },
+  container: {
+    paddingRight: theme.wrapper.marginHorizontal,
+    paddingTop: 25 + Constants.statusBarHeight,
+    position: 'absolute',
+    right: 0,
+    top: 0,
+  },
   contentContainer: {
     flexGrow: 1,
   },
   scrollView: {
     flex: 1,
+    zIndex: -1,
   },
 });
 
