5b82e09258cceefe48e8429e5282a1521e3fb9bf
AZIHubert <allan.aoudji@protonmail.com>
2021-02-03 12:20:33 +0100

    add not allow special chars for username

--- src/helpers/formErrors/index.ts
+++ src/helpers/formErrors/index.ts
@@ -1,6 +1,7 @@
 export const CONFIRM_PASSWORD = 'must match password';
 export const EMAIL_FIELD = 'should be a valid email';
 export const HAS_SPACES = 'can not contain spaces';
+export const HAS_SPECIAL_CHARS = 'can not contain special char';
 export const MAX_LENGTH_THRITY = 'should have a maximum length of 30';
 export const MAX_LENGTH_TWO_HUNDRER = 'should have a maximum length of 200';
 export const MIN_LENGTH_OF_HEIGH = 'should have a minimum length of 8';
